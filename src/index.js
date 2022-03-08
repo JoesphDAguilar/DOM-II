44f6708adb327769f496618c5ec1de60dce0f292
JoesphAguilar <josephjd93@gmail.com>
2022-03-08 11:34:43 -0800

    Added 2nd event listener

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import './less/index.less'
 
 // Your code goes here!
 
-// Event Listener 1
+// Event Listener 1; Console logs message when clicking header img
 const funBus = document.querySelector('header img');
 funBus.addEventListener("click", function(evt) {
     console.log('Welcome to the fun bus!');
 })
 
-// Event Listener 2
+// Event Listener 2; changes logo-heading font color to yellow
+const logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener("mouseover", function(evt) {
+    evt.target.style.color = 'yellow';
+    setTimeout(function() {
+        evt.target.style.color = "";
+      }, 1000);
+    }, false);
+
 
 // Event Listener 3
 
