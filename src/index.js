8ac030ca470d71fdf5f74e73ef113d51a0f7a571
JoesphAguilar <josephjd93@gmail.com>
2022-03-08 11:15:16 -0800

    Organized index.js, created first event listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './less/index.less'
 
 // Your code goes here!
+
+// Event Listener 1
+const funBus = document.querySelector('header img');
+funBus.addEventListener("click", function(evt) {
+    console.log('Welcome to the fun bus!');
+})
+
+// Event Listener 2
+
+// Event Listener 3
+
+// Event Listener 4
+
+// Event Listener 5
+
+// Event Listener 6
+
+// Event Listener 7
+
+// Event Listener 8
+
+// Event Listener 9
+
+// Event Listener 10
